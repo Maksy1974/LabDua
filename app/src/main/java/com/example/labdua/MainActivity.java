16a5e40a8c3c1445fc0d0a3391f10b5707b8b0bc
Maksy Sendiang <maksy@elektro.polimdo.ac.id>
2023-06-26 16:31:41 +0800

    Perubahan Kedua

--- app/src/main/java/com/example/labdua/MainActivity.java
+++ app/src/main/java/com/example/labdua/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.labdua;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.optionmenu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.buka)
+        {
+          Toast.makeText(this,"Welcome Buka",Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
